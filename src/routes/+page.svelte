54223fe77f2fd7602991e451a1626bfffa110c99
Anders Sjöberg <anders.sjoberg@calabrio.com>
2022-10-16 02:45:46 +0200

    Time to event, live updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,29 @@
+<script>
+	import { sv } from 'date-fns/locale';
+	import { onDestroy, onMount } from 'svelte';
+	import { formatDistance } from 'date-fns';
+
+	const theDate = new Date(2023, 3, 6, 12, 0, 0, 0);
+	let currentTime = new Date();
+
+	let timer;
+
+	onMount(() => {
+		timer = setInterval(() => {
+			currentTime = new Date();
+		}, 1000);
+	});
+
+	onDestroy(() => {
+		clearInterval(timer);
+	});
+</script>
+
 <div class="page">
 	<h1>Gävle Skivmässa</h1>
-	<h2>6:e maj 2023</h2>
+	<h2>
+		6:e maj 2023 kl 12:00, om {formatDistance(theDate, currentTime, { locale: sv })}
+	</h2>
 	<div class="posterContainer">
 		<img src="/POSTER SKARP.jpg" alt="poster" />
 	</div>
