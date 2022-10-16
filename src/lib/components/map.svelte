baf2f5d9c3055ed611e83129cf4d0ef6c88adfad
Anders Sjöberg <anders.sjoberg@calabrio.com>
2022-10-16 03:06:34 +0200

    Adaptive map

--- src/lib/components/map.svelte
+++ src/lib/components/map.svelte
@@ -2,7 +2,7 @@
 <iframe
 	title="hitta hit"
 	src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1953.8621497774147!2d17.17606531631538!3d60.678342109172604!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4660c68f6de57d55%3A0x314d51adc6243d5b!2sGasklockorna%20G%C3%A4vle!5e0!3m2!1ssv!2sse!4v1665880112838!5m2!1ssv!2sse"
-	width="500"
+	width="90%"
 	height="450"
 	style="border:0;"
 	allowfullscreen={true}
