a02d4da26524473a0abecb9e41bd2aa005ad5fe4
Anders Sjöberg <anders.sjoberg@calabrio.com>
2022-10-16 02:34:15 +0200

    Map

--- src/routes/exhibitors/+page.svelte
+++ src/routes/exhibitors/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	import Map from '$lib/components/map.svelte';
+</script>
+
 <div>
 	<h1>Information för utställare</h1>
 	<p>
@@ -5,4 +9,5 @@
 		ipsam temporibus suscipit nisi a officiis vero veritatis, commodi, aperiam nemo in maxime est
 		modi dolorem repudiandae.
 	</p>
+	<Map />
 </div>
