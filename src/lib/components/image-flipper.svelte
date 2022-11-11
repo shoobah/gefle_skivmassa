b125d4f3d9a4e032c1fb9a2a3805a1d512984a60
Anders Sjöberg <anders.sjoberg@calabrio.com>
2022-11-11 22:13:20 +0100

    More stuff

--- src/lib/components/image-flipper.svelte
+++ src/lib/components/image-flipper.svelte
@@ -15,41 +15,53 @@
 
 	let innerWidth = 2048;
 	let index = 0;
+	let timer;
 
 	$: sliderWidth = innerWidth > 1366 ? size : innerWidth - 20;
 	$: index = index % imageList.length;
 	$: nextIndex = (index + 1) % imageList.length;
-	$: firstVisible = index % 2 === 0;
 
 	let opacity1 = tweened(1, { duration: 500 });
 	let opacity2 = tweened(0, { duration: 500 });
 
-	$: $opacity1 = firstVisible ? 1 : 0;
-	$: $opacity2 = firstVisible ? 0 : 1;
-
-	$: $opacity1 === 0 ? moveToNext() : null;
-	$: $opacity2 === 0 ? moveToNext() : null;
-
 	function moveToNext() {
-		setTimeout(() => (index += 1), interval);
+		setTimeout(() => {
+			index += 1;
+			$opacity1 === 0 ? 1 : 0;
+			$opacity2 === 1 ? 0 : 1;
+		}, interval);
 	}
 
 	function moveToPrev() {
 		index -= 1;
 	}
 
-	onMount(() => {});
+	onMount(() => {
+		timer = setInterval(() => {
+			moveToNext();
+		}, interval * 2);
+	});
+
+	onDestroy(() => {
+		clearInterval(timer);
+	});
 </script>
 
 <svelte:window bind:innerWidth />
 
-<pre>
+<!-- <pre>
     {firstVisible}
-    {index}
-    {nextIndex}
-</pre>
-<div on:click={moveToNext} on:keydown={moveToNext}>
+    {index + ': ' + $opacity1}
+    {nextIndex + ': ' + $opacity2}
+</pre> -->
+<div
+	class="flipper-div"
+	style="width: {sliderWidth}px; height: {sliderWidth * ar}px;"
+	on:click={moveToNext}
+	on:keydown={moveToNext}
+>
 	<img
+		class="flipper-image"
 		style="opacity: {$opacity1}"
 		src={imageList[index].src}
 		alt={imageList[index].alt}
@@ -57,6 +69,7 @@
 		height={sliderWidth * ar}
 	/>
 	<img
+		class="flipper-image"
 		style="opacity: {$opacity2}"
 		src={imageList[nextIndex].src}
 		alt={imageList[nextIndex].alt}
@@ -66,11 +79,11 @@
 </div>
 
 <style>
-	div {
+	.flipper-div {
 		position: relative;
 	}
 
-	img {
+	.flipper-image {
 		position: absolute;
 	}
 </style>
