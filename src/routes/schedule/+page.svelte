08f526f4ac23b04a518ca1747deae557934a1f82
Anders Sjöberg <shoobah@gmail.com>
2023-02-22 23:39:54 +0100

    Schema text done

--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -1,16 +1,187 @@
 <table>
 	<thead>
 		<tr>
+			<th />
 			<th class="dark-blue">Stora Gasklockan</th>
 			<th class="dark-orange">Retorten</th>
 			<th class="dark-green">Scen Retorten</th>
 			<th class="dark-pink">Scen Gasklockan</th>
 		</tr>
 	</thead>
-	<tbody />
+	<tbody>
+		<tr>
+			<td />
+			<td class="light-blue">Entré: 50kr / förköp</td>
+			<td class="light-orange">Fri entré</td>
+			<td class="light-green">Fri entré</td>
+			<td class="light-pink">Entré: 400 / förköp</td>
+		</tr>
+		<tr />
+		<tr>
+			<td class="bold">12:00</td>
+			<td rowspan="12" class="light-blue event">
+				<strong> Skivmässa </strong><br />
+				<span> ca 150 meter skivbord<br />Fikaförsäljning </span></td
+			>
+			<td rowspan="12" class="light-orange event">
+				<strong> Musikloppis </strong><br />
+				<span>
+					Fynda instrument,<br />
+					tillbehör och mycket<br />
+					annat i musikväg
+				</span></td
+			>
+			<td rowspan="2" class="light-green event">
+				<strong> Restaurangen håller<br />öppet hela dagen</strong><br />
+				<span>
+					Fynda instrument,<br />
+					tillbehör och mycket<br />
+					annat i musikväg
+				</span></td
+			>
+		</tr>
+		<tr>
+			<td class="cursive">12:30</td>
+		</tr>
+		<tr>
+			<td class="bold">13:00</td>
+			<td rowspan="2" class="light-green event"> kl 13:00<br /><span class="bold">BAND 1</span></td>
+		</tr>
+		<tr>
+			<td class="cursive">13:30</td>
+		</tr>
+		<tr>
+			<td class="bold">14:00</td>
+		</tr>
+		<tr>
+			<td class="cursive">14:30</td>
+		</tr>
+		<tr>
+			<td class="bold">15:00</td>
+			<td rowspan="2" class="light-green event"> kl 15:00<br /><span class="bold">BAND 2</span></td>
+		</tr>
+		<tr>
+			<td class="cursive">15:30</td>
+		</tr>
+		<tr>
+			<td class="bold">16:00</td>
+		</tr>
+		<tr>
+			<td class="cursive">16:30</td>
+		</tr>
+		<tr>
+			<td class="bold">17:00</td>
+			<td rowspan="2" class="light-green event"> kl 17:00<br /><span class="bold">BAND 3</span></td>
+		</tr>
+		<tr>
+			<td class="cursive">17:30</td>
+		</tr>
+		<tr>
+			<td class="bold">18:00</td>
+			<td class="dark-blue">
+				<strong> 18:00 Stänger </strong>
+			</td>
+			<td class="dark-orange">
+				<strong> 18:00 Stänger </strong>
+			</td>
+			<td />
+			<td class="dark-pink bold"> 18:00 Öppnar </td>
+		</tr>
+		<tr>
+			<td class="cursive">18:30</td>
+			<td />
+			<td />
+			<td />
+			<td rowspan="2" class="light-pink event">
+				kl 18:30<br />
+				<span class="bold">Children of the Sün</span>
+			</td>
+		</tr>
+		<tr>
+			<td class="bold">19:00</td>
+			<td />
+			<td />
+			<td rowspan="2" class="light-green event"> kl 19:00<br /><span class="bold">BAND 4</span></td>
+		</tr>
+		<tr>
+			<td class="cursive">19:30</td>
+			<td />
+			<td />
+			<td rowspan="2" class="light-pink event">
+				kl 19:30<br />
+				<span class="bold">Vokonis</span>
+			</td>
+		</tr>
+		<tr>
+			<td class="bold">20:00</td>
+		</tr>
+		<tr>
+			<td class="cursive">20:30</td>
+			<td />
+			<td />
+			<td />
+			<td rowspan="2" class="light-pink event">
+				kl 20:40<br />
+				<span class="bold">Horndal</span>
+			</td>
+		</tr>
+		<tr>
+			<td class="bold">21:00</td>
+			<td class="dark-purple bold">Musikhuset</td>
+			<td />
+			<td rowspan="2" class="light-green event"> kl 21:00<br /><span class="bold">BAND 5</span></td>
+		</tr>
+		<tr>
+			<td class="cursive">21:30</td>
+			<td rowspan="7" class="light-purple event">
+				Fri entré - först till kvarn<br /><br />
+				<strong>Efterfest på stan</strong><br />
+				Fri entré med entréband<br />från skivmässan
+			</td>
+		</tr>
+		<tr>
+			<td class="bold">22:00</td>
+			<td />
+			<td />
+			<td rowspan="2" class="light-pink event">
+				kl 22:00<br />
+				<span class="bold">Witchcraft</span>
+			</td>
+		</tr>
+		<tr>
+			<td class="cursive">22:30</td>
+		</tr>
+		<tr>
+			<td class="bold">23:00</td>
+		</tr>
+		<tr>
+			<td class="cursive">23:30</td>
+		</tr>
+		<tr>
+			<td class="bold">00:00</td>
+		</tr>
+		<tr>
+			<td class="cursive">00:30</td>
+		</tr>
+		<tr>
+			<td class="bold">01:00</td>
+			<td rowspan="7" class="light-purple event">
+				<strong>Stänger</strong>
+			</td>
+		</tr>
+	</tbody>
 </table>
 
 <style>
+	table {
+		width: 100%;
+		color: #fff;
+	}
+
+	.event {
+		vertical-align: top;
+		padding: 5px;
+	}
 	.dark-blue {
 		background-color: #4f81bd;
 		color: #000;
@@ -49,5 +220,22 @@
 	.light-pink {
 		background-color: #f2dcdb;
 		color: #000;
+	}
+	.dark-purple {
+		background-color: #b1a0c7;
+		color: #000;
+	}
+
+	.light-purple {
+		background-color: #e4dfec;
+		color: #000;
+	}
+
+	.bold {
+		font-weight: bold;
+	}
+
+	.cursive {
+		font-style: italic;
 	}
 </style>
