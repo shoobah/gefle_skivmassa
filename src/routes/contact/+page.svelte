0abd70648d623fbff13e53807042a8dbed0091d1
Anders Sjöberg <shoobah@gmail.com>
2023-12-05 21:19:52 +0100

    Updated info

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,60 +1,46 @@
 <script>
-	import { state } from '$lib/stores/state.js';
-	import Map from '$lib/components/map.svelte';
+  import { state } from '$lib/stores/state.js';
+  import Map from '$lib/components/map.svelte';
 </script>
 
 <svelte:head>
-	<meta name="description" content="Gefle Skivmässa. Kontakt" />
+  <meta name="description" content="Gefle Skivmässa. Kontakt" />
 </svelte:head>
 
 <div>
-	<h1>Kontakt</h1>
-	<article>
-		Mail: <a href="mailto:info@soundsfair.com">info@soundsfair.com</a><br />
-		Facebook:
-		<a href="https://www.facebook.com/gefleskivmassa">www.facebook.com/gefleskivmassa</a><br />
-		Instagram:
-		<a href="https://www.instagram.com/gefleskivmassa">www.instagram.com/gefleskivmassa</a><br />
-		<br />
-		<h2>Samarbetspartners</h2>
-		<a href="https://www.medborgarskolan.se" target="_blank" rel="noreferrer">
-			Medborgarskolan
-		</a><br />
-		<a href="https://www.gavle.se/gasklockorna" target="_blank" rel="noreferrer"> Gasklockorna </a>
-		<br />
-		<a
-			href="https://www.regiongavleborg.se/kultur/verksamhet/musik/"
-			target="_blank"
-			rel="noreferrer"
-		>
-			Region Gävleborg, musik
-		</a>
-		<br />
-		<a href="https://www.folkbildningsforum.se/" target="_blank" rel="noreferrer">
-			Folkbildninsforum
-		</a>
-		<br />
-		<a href="https://musikhuset.nu/" target="_blank" rel="noreferrer"> Musikhuset </a>
-		<Map />
-	</article>
+  <h1>Kontakt</h1>
+  <article>
+    Mail: <a href="mailto:info@soundsfair.com">info@soundsfair.com</a><br />
+    Facebook:
+    <a href="https://www.facebook.com/gefleskivmassa">www.facebook.com/gefleskivmassa</a><br />
+    Instagram:
+    <a href="https://www.instagram.com/gefleskivmassa">www.instagram.com/gefleskivmassa</a><br />
+    <br />
+    <h2>Samarbetspartners</h2>
+    <a href="https://www.medborgarskolan.se" target="_blank" rel="noreferrer"> Medborgarskolan </a><br />
+    <a href="https://www.gavle.se/gasklockorna" target="_blank" rel="noreferrer"> Gasklockorna </a>
+    <br />
+    <a href="https://www.bilda.nu/" target="_blank" rel="noreferrer"> Bilda </a>
+    <Map />
+  </article>
 </div>
 
 <style>
-	img {
-		border: none;
-		width: 20vw;
-		max-width: 200px;
-		min-width: 100px;
-	}
+  img {
+    border: none;
+    width: 20vw;
+    max-width: 200px;
+    min-width: 100px;
+  }
 
-	div {
-		width: 50vw;
-		padding-bottom: 20px;
-	}
+  div {
+    width: 50vw;
+    padding-bottom: 20px;
+  }
 
-	@media (max-width: 820px) {
-		div {
-			width: 90vw;
-		}
-	}
+  @media (max-width: 820px) {
+    div {
+      width: 90vw;
+    }
+  }
 </style>
