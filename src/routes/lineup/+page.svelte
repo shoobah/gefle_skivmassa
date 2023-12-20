26cdf4356def2ac0a41706d93bd431accc1b6dcf
Anders Sjöberg <anders.sjoberg@sileon.com>
2023-12-20 11:56:42 +0100

    Sorting

--- src/routes/lineup/+page.svelte
+++ src/routes/lineup/+page.svelte
@@ -4,27 +4,6 @@
 
 <div>
   <article>
-    <img src="/images/Beardfish/Logo.png" alt="Beardfish logo" />
-    <p>
-      <a href={$state.ticketLink} target="_blank"> Beardfish spelar lördag 4:e maj </a>
-    </p>
-    <p>
-      Gävlebandet Beardfish har sedan starten 2001 levererat flertalet album med delikat progressiv rock. De turnerade
-      flitigt i stora delar av Europa, USA och Sydamerika men valde efter albumet ”+4626 COMFORTZONE” att lägga ner
-      bandet 2016.
-    </p>
-    <p>
-      Sex år senare, alltså 2022, väcktes ändå bandet till liv igen. Rikard hade skrivit nya låtar och de andades
-      Beardfish rätt igenom. Efter världshistoriens kortaste överläggning med övriga i bandet, ser vi alltså åter
-      Beardfish på livescenerna igen! En ny skiva spelas in i detta nu och innan de beger sig till We Love Rock-
-      festivalen i Norge och Night of the Progg i Tyskland ses de på Gefle Skivmässas scen under lördagen 4 maj. Räkna
-      med ett spelsuget band, full med ny energi och nya låtar.
-    </p>
-    <p>Beardfish är Rikard Sjöblom, David Zackrison, Robert Hansen och Magnus Östgren.</p>
-    <p>&nbsp;</p>
-    <img src="/images/Beardfish/Beardfish 2023 in studio.jpg" alt="Beardfish" />
-  </article>
-  <article>
     <img src="/images/Skraeckoedlan/logotype.png" alt="Skraeckoedlan logo" />
     <p>
       <a href={$state.ticketLink} target="_blank"> Skraeckoedlan spelar fredag 3:e maj </a>
@@ -47,6 +26,28 @@
     <p>Skraeckoedlan presenteras i samarbete med Blackbird Artists</p>
 
     <img src="/images/Skraeckoedlan/Skraeckoedlan-2-Foto Mats Ek.jpg" alt="Skraeckoedlan" />
+  </article>
+  <hr />
+  <article>
+    <img src="/images/Beardfish/Logo.png" alt="Beardfish logo" />
+    <p>
+      <a href={$state.ticketLink} target="_blank"> Beardfish spelar lördag 4:e maj </a>
+    </p>
+    <p>
+      Gävlebandet Beardfish har sedan starten 2001 levererat flertalet album med delikat progressiv rock. De turnerade
+      flitigt i stora delar av Europa, USA och Sydamerika men valde efter albumet ”+4626 COMFORTZONE” att lägga ner
+      bandet 2016.
+    </p>
+    <p>
+      Sex år senare, alltså 2022, väcktes ändå bandet till liv igen. Rikard hade skrivit nya låtar och de andades
+      Beardfish rätt igenom. Efter världshistoriens kortaste överläggning med övriga i bandet, ser vi alltså åter
+      Beardfish på livescenerna igen! En ny skiva spelas in i detta nu och innan de beger sig till We Love Rock-
+      festivalen i Norge och Night of the Progg i Tyskland ses de på Gefle Skivmässas scen under lördagen 4 maj. Räkna
+      med ett spelsuget band, full med ny energi och nya låtar.
+    </p>
+    <p>Beardfish är Rikard Sjöblom, David Zackrison, Robert Hansen och Magnus Östgren.</p>
+    <p>&nbsp;</p>
+    <img src="/images/Beardfish/Beardfish 2023 in studio.jpg" alt="Beardfish" />
   </article>
 </div>
 
