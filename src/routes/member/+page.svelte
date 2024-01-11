de7e53e31cac87eb3e2946fdaadbcf8099a54c4b
Anders Sjöberg <anders.sjoberg@sileon.com>
2024-01-11 17:28:55 +0100

    Member page

---
+++ src/routes/member/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+  /** @type {import('./$types').PageData} */
+  export let data;
+</script>
+
+<div>
+  <article>
+    <h1>Stödmedlem</h1>
+    <p></p>
+    <h2>Vill du bli medlem i den ideella Kulturföreningen Sounds Fair?</h2>
+    <br />
+    <p>
+      Hjälp oss att växa och stöd den lokala arrangörskulturen genom att bli stödmedlem i föreningen Sounds Fair som
+      arrangerar Gefle Skivmässa. Som medlem läggs du till i vår e-postlista och får info om våra arrangemang. Vi siktar
+      även på att komma med erbjudanden kring biljetter, merch och andra samarbeten längre fram.
+    </p>
+    <p>
+      Som betalande medlem ingår entré till skivmässan lördag 4 maj (värde 50 kr) under eventet Gefle Skivmässa 3-4 maj.
+    </p>
+    <h2>För att bli medlem</h2>
+    <ol>
+      <li>Sätt in 100 kr på Bankgiro 381-4480 och ange ditt namn vid betalning</li>
+      <li>Skicka ett mail till: <a href="mailto:info@soundsfair.se">info@soundsfair.se</a> och meddela oss att du betalat</li>
+    </ol>
+    <p>Vi lägger till din e-postadress i vår e-postlista</p>
+    <h2>Kontakt och info</h2>
+    <p>
+      Kulturföreningen Sounds Fair c/o Emil Alstermark Stormgatan 76 802 75 Gävle Organisationsnummer 802544-0911
+      BankGiro: 381-4480 E-post: info@soundsfair.se
+    </p>
+    <h2>Styrelse</h2>
+    <p>
+      Ordförande: Anders Sjöberg<br />
+      Ledamöter: Jerry van Kooten och Emil Alstermark <br />
+      <br />
+      Revisor: Lena-Mie Bernkert
+    </p>
+  </article>
+</div>
