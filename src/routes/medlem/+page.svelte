918288fdd5235aa4b560a0972b52dda3c5952f7c
Anders Sjöberg <anders.sjoberg@sileon.com>
2024-01-11 18:18:57 +0100

    Board image

--- src/routes/medlem/+page.svelte
+++ src/routes/medlem/+page.svelte
@@ -1,7 +1,7 @@
 <div>
-    <div>
-        <img src="/icons/Sounds_Fair_vit_cirkel.png" alt="Skivmässa" />
-    </div>
+  <div>
+    <img class="logo" src="/icons/Sounds_Fair_vit_cirkel.png" alt="Skivmässa" />
+  </div>
   <article>
     <h1>Stödmedlem</h1>
     <p></p>
@@ -9,27 +9,30 @@
     <br />
     <p>
       Hjälp oss att växa och stöd den lokala arrangörskulturen genom att bli stödmedlem i föreningen Sounds Fair som
-      arrangerar Gefle Skivmässa. Som stödmedlem läggs du till i vår e-postlista och får info om våra arrangemang. Vi siktar
-      även på att komma med erbjudanden kring biljetter, merch och andra samarbeten längre fram.
+      arrangerar Gefle Skivmässa. Som stödmedlem läggs du till i vår e-postlista och får info om våra arrangemang. Vi
+      siktar även på att komma med erbjudanden kring biljetter, merch och andra samarbeten längre fram.
     </p>
     <p>
-      Som betalande stödmedlem ingår entré till skivmässan lördag 4 maj (värde 50 kr) under eventet Gefle Skivmässa 3-4 maj.
+      Som betalande stödmedlem ingår entré till skivmässan lördag 4 maj (värde 50 kr) under eventet Gefle Skivmässa 3-4
+      maj.
     </p>
     <h2>För att bli stödmedlem</h2>
     <ol>
       <li>Sätt in 100 kr på Bankgiro 381-4480 och ange ditt namn vid betalning</li>
-      <li>Skicka ett mail till: <a href="mailto:info@soundsfair.se">info@soundsfair.se</a> och meddela oss att du betalat</li>
+      <li>
+        Skicka ett mail till: <a href="mailto:info@soundsfair.se">info@soundsfair.se</a> och meddela oss att du betalat
+      </li>
     </ol>
     <p>Vi lägger till din e-postadress i vår e-postlista</p>
     <h2>Kontakt och info</h2>
     <p>
-      Kulturföreningen Sounds Fair<br/>
-      c/o Emil Alstermark<br/>
-      Stormgatan 76<br/>
-      802 75 Gävle<br/>
-      <br/>
-      Organisationsnummer: 802544-0911<br/>
-      BankGiro: 381-4480<br/>
+      Kulturföreningen Sounds Fair<br />
+      c/o Emil Alstermark<br />
+      Stormgatan 76<br />
+      802 75 Gävle<br />
+      <br />
+      Organisationsnummer: 802544-0911<br />
+      BankGiro: 381-4480<br />
       E-post: <a href="mailto:info@soundsfair.se">info@soundsfair.se</a>
     </p>
     <h2>Styrelse</h2>
@@ -40,19 +43,54 @@
       Revisor: Lena-Mie Bernkert
     </p>
   </article>
+  <div class="board_image">
+    <div class="board">
+      <img src="/images/sounds_fair_board.jpg" alt="Skivmässa" />
+      <div class="text">
+        <sub>Styrelsen: Emil, Anders, Jerry. Foto: Johan Öbrink</sub>
+      </div>
+    </div>
+  </div>
 </div>
 
 <style>
-img {
-  width: 30vw;
-  max-width: 300px;
-  margin: 10px auto;
-  display: block;
-}
+  .board_image {
+    display: grid;
+    width: 50vw;
+  }
 
-@media (max-width: 888px) {
-    img {
-        width: 50vw;
+  .board_image img {
+    width: 100%;
+  }
+  .text {
+    text-align: right;
+  }
+
+  sub {
+    align-self: flex-end;
+  }
+  .logo {
+    width: 30vw;
+    max-width: 300px;
+    margin: 10px auto;
+    display: block;
+  }
+  .board {
+    margin: 10px auto;
+    display: block;
+  }
+
+  @media (max-width: 888px) {
+    .logo {
+      width: 50vw;
     }
-}
+
+    .board_image {
+      width: 100vw;
+    }
+
+    .text {
+      text-align: center;
+    }
+  }
 </style>
