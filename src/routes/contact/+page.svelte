595e4e95b4b932de9a731ea4972f806bcf02da7f
Anders Sjöberg <anders.sjoberg@sileon.com>
2024-03-04 15:03:11 +0100

    💩 Trying to get apple maps working

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,7 +1,46 @@
 <script>
-  import { state } from '$lib/stores/state.js';
-  import Map from '$lib/components/map.svelte';
+  import { onMount } from "svelte";
+  import { state } from "$lib/stores/state.js";
+  import Map from "$lib/components/map.svelte";
+
+  onMount(() => {
+    mapkit.init({
+      authorizationCallback: function (done) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "/services/jwt");
+        xhr.addEventListener("load", function () {
+          done(this.responseText);
+        });
+        xhr.send();
+      },
+    });
+
+    var Cupertino = new mapkit.CoordinateRegion(
+      new mapkit.Coordinate(37.3316850890998, -122.030067374026),
+      new mapkit.CoordinateSpan(0.167647972, 0.354985255)
+    );
+    var map = new mapkit.Map("map");
+    map.region = Cupertino;
+  });
 </script>
+
+<style>
+  div {
+    width: 50vw;
+    padding-bottom: 20px;
+  }
+
+  #map {
+    width: 100%;
+    height: 600px;
+  }
+
+  @media (max-width: 820px) {
+    div {
+      width: 90vw;
+    }
+  }
+</style>
 
 <svelte:head>
   <meta name="description" content="Gefle Skivmässa. Kontakt" />
@@ -22,18 +61,6 @@
     <br />
     <a href="https://www.bilda.nu/" target="_blank" rel="noreferrer"> Bilda </a>
     <Map />
+    <div id="map"></div>
   </article>
 </div>
-
-<style>
-  div {
-    width: 50vw;
-    padding-bottom: 20px;
-  }
-
-  @media (max-width: 820px) {
-    div {
-      width: 90vw;
-    }
-  }
-</style>
