8aefba5bc4206dcbb50bce11b15692c91b395da2
Anders Sjöberg <anders@jotib.se>
2024-12-08 19:59:45 +0100

    Amazon SES

--- src/lib/mailer.js
+++ src/lib/mailer.js
@@ -1,20 +1,36 @@
-import postmark from "postmark";
+import AWS from "aws-sdk";
 
 export const sendMail = async (
   /** @type {string} */ body,
   /** @type {string} */ subject,
 ) => {
   try {
-    var client = new postmark.ServerClient(
-      import.meta.env.VITE_POSTMARK_API_KEY,
-    );
-    await client.sendEmail({
-      From: import.meta.env.VITE_SMTP_FROM,
-      To: import.meta.env.VITE_SMTP_TO,
-      Subject: subject,
-      TextBody: body,
-      MessageStream: "outbound",
+    AWS.config.update({
+      accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
+      secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,
+      region: import.meta.env.VITE_AWS_REGION,
     });
+
+    const ses = new AWS.SES({ apiVersion: "2010-12-01" });
+
+    const params = {
+      Source: import.meta.env.VITE_SMTP_FROM,
+      Destination: {
+        ToAddresses: [import.meta.env.VITE_SMTP_TO],
+      },
+      Message: {
+        Subject: {
+          Data: subject,
+        },
+        Body: {
+          Text: {
+            Data: body,
+          },
+        },
+      },
+    };
+
+    await ses.sendEmail(params).promise();
   } catch (error) {
     console.log("Error while sending email", error);
     return { success: false };
